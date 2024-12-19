de173824b254d992f32e7ccbe1a5a6ce7e64cd84
Vedant Tuvar <152903617+VedantTuvar@users.noreply.github.com>
2024-12-19 21:00:35 +0530

    Login Page Added

---
+++ JS/Login.js
@@ -0,0 +1,31 @@
+const sign_in_btn = document.querySelector("#sign-in-btn");
+const sign_up_btn = document.querySelector("#sign-up-btn");
+const container = document.querySelector(".container");
+
+sign_up_btn.addEventListener("click", () => {
+  container.classList.add("sign-up-mode");
+});
+
+sign_in_btn.addEventListener("click", () => {
+  container.classList.remove("sign-up-mode");
+});
+
+const togglePassword = (toggleId, passwordId) => {
+  const toggleIcon = document.querySelector(toggleId);
+  const passwordField = document.querySelector(passwordId);
+
+  if (toggleIcon && passwordField) {
+    toggleIcon.addEventListener("click", () => {
+      const type =
+        passwordField.getAttribute("type") === "password" ? "text" : "password";
+      passwordField.setAttribute("type", type);
+
+      toggleIcon.classList.toggle("fa-eye");
+      toggleIcon.classList.toggle("fa-eye-slash");
+    });
+  }
+};
+
+togglePassword("#toggle-login-password", "#login-password");
+
+togglePassword("#toggle-signup-password", "#signup-password");
